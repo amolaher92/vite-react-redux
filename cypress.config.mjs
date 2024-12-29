eadfa4e133bb77f9b828ec26bf1b6336969ee322
amolaher92 <amolaher92@gmail.com>
2024-12-29 16:10:25 +0530

    added cypress test

---
+++ cypress.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts,jsx,tsx}',
+    supportFile: 'cypress/support/e2e.ts',
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+    supportFile: false, 
+  },
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
     environment: 'jsdom', // Simulates a browser-like DOM environment
     setupFiles: './src/setupTests.ts', // Path to setup file for test configuration
     css: true, // Enable CSS support in tests
-    watch:false,
+    watch:true,
     coverage: {
       provider:'v8',
       reporter: ['text', 'json', 'html'], // Generate coverage reports
       include:['src/**/*.{ts,tsx}'],
       exclude: ['node_modules/', 'src/setupTests.ts','src/**/*.{test,spec,stories}.{ts,tsx}'], // Exclude these files from coverage
     },
-    testMatch:['**/*.test.{ts,tsx}','**/*.spec.{ts,tsx}'],
+    //testMatch:['**/*.test.{ts,tsx}','**/*.spec.{ts,tsx}'],
   },
   resolve: {
     alias: {
