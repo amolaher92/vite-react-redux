dd63f75fe9932efaebb9eb89b774f381ecf20797
amolaher92 <amolaher92@gmail.com>
2024-12-29 16:23:21 +0530

    added eslint and prettier

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,41 +1,45 @@
-import { render, screen, fireEvent } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
-import App from '../App'
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from '../App';
 
 describe('App Component', () => {
   it('renders Vite and React logos', () => {
-    render(<App />)
-    const viteLogo = screen.getByAltText('Vite logo')
-    const reactLogo = screen.getByAltText('React logo')
+    render(<App />);
+    const viteLogo = screen.getByAltText('Vite logo');
+    const reactLogo = screen.getByAltText('React logo');
 
-    expect(viteLogo).toBeInTheDocument()
-    expect(reactLogo).toBeInTheDocument()
-  })
+    expect(viteLogo).toBeInTheDocument();
+    expect(reactLogo).toBeInTheDocument();
+  });
 
   it('renders the initial count value', () => {
-    render(<App />)
-    const button = screen.getByRole('button', { name: /count is 0/i }) // Use getByRole to retrieve a single button
+    render(<App />);
+    const button = screen.getByRole('button', { name: /count is 0/i }); // Use getByRole to retrieve a single button
 
-    expect(button).toBeInTheDocument()
-  })
+    expect(button).toBeInTheDocument();
+  });
 
   it('increments count value on button click', () => {
-    render(<App />)
-    const button = screen.getByRole('button', { name: /count is 0/i }) // Use getByRole
+    render(<App />);
+    const button = screen.getByRole('button', { name: /count is 0/i }); // Use getByRole
 
-    fireEvent.click(button)
+    fireEvent.click(button);
 
-    expect(button.textContent).toBe('count is 1')
-  })
+    expect(button.textContent).toBe('count is 1');
+  });
 
   it('renders documentation links', () => {
-    render(<App />)
-    const links = screen.getAllByRole('link')
+    render(<App />);
+    const links = screen.getAllByRole('link');
 
-    const viteLink = links.find((link) => link.getAttribute('href') === 'https://vite.dev')
-    const reactLink = links.find((link) => link.getAttribute('href') === 'https://react.dev')
+    const viteLink = links.find(
+      (link) => link.getAttribute('href') === 'https://vite.dev',
+    );
+    const reactLink = links.find(
+      (link) => link.getAttribute('href') === 'https://react.dev',
+    );
 
-    expect(viteLink).toBeTruthy()
-    expect(reactLink).toBeTruthy()
-  })
-})
+    expect(viteLink).toBeTruthy();
+    expect(reactLink).toBeTruthy();
+  });
+});
