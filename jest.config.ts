81d3d8a4e07507e96ab5a5aa9be49f3cd6d648d9
amolaher92 <amolaher92@gmail.com>
2024-12-29 19:04:54 +0530

    updated jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'jest';
 const config: Config = {
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+	setupFilesAfterEnv: ['@testing-library/jest-dom'],
 	transform:{
 		'^.+\\.tsx?$':'ts-jest',
 	},
